ccdb4e4a2ae059d7b84f546d52fe1c6b8bb9ac0a
Tuukka Toivonen <tuukka.toivonen@iki.fi>
2025-06-02 20:34:14 +0300

    llm.py: add new class without any streaming
    Rename class Llm to LlmStreaming and make new class Llm
    which doesn't stream at all.

--- llm.py
+++ llm.py
@@ -24,6 +24,9 @@ class Llm:
         self._tokens_m = 1.1139
         self._tokens_b = -26.207
 
+    def __del__(self):
+        self._session.close()
+
     def _reset_stats(self):
         self._stats = {
             'model': None,
@@ -31,43 +34,30 @@ class Llm:
             'timings': {},          # timings not available through LiteLLM
         }
 
-    def __del__(self):
-        self._session.close()
+    def _parse_stats(self, chunk):
+        for k in self._stats.keys():
+            if k in chunk:
+                self._stats[k] = chunk[k]
 
     def completion(self, messages):
         self._reset_stats()
         payload = self._options.copy()
-        payload['stream'] = True
-        payload['stream_options'] = { 'include_usage': True }     # Required for LiteLLM
         payload['messages'] = messages
 
         response = self._session.post(
             self._base_url + '/v1/chat/completions',
             json = payload,
-            stream = True,
             verify = not self._insecure
         )
         response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
-        iterator = response.iter_lines()
+        response = response.json()
+        self._parse_stats(response)
 
-        for line_bytes in iterator:
-            line = line_bytes.decode('utf-8').strip()
-            # Standard SSE format: lines start with "data: "
-            if line.startswith('data:'):
-                data_content = line[len('data:'):].strip()
-                if data_content == '[DONE]': # OpenAI-specific end-of-stream marker
-                    response.close()
-                    return
-                chunk = json.loads(data_content)
-                if ('choices' in chunk and
-                    'delta' in chunk['choices'][0] and
-                    'content' in chunk['choices'][0]['delta']):
-                    yield chunk['choices'][0]['delta']['content']
-                for k in self._stats.keys():
-                    if k in chunk:
-                        self._stats[k] = chunk[k]
-            # Lines that are not "data: ..." or empty lines are typically ignored in SSE
-            # or could be comments (starting with ':')
+        if ('choices' in response and
+            'message' in response['choices'][0] and
+            'content' in response['choices'][0]['message']):
+            return response['choices'][0]['message']['content']
+        raise Exception('API returned bad format')
 
     def completion_stats(self):
         return self._stats
@@ -110,7 +100,47 @@ class Llm:
         response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
         return response.json()['data'][0]['embedding']
 
-class LlmLineStreaming(Llm):
+class LlmStreaming(Llm):
+    def __init__(self, url, api_key=None, options={}, embedding_query='', insecure=False):
+        loc = locals()
+        del loc['self']
+        del loc['__class__']
+        super().__init__(**loc)
+
+    def completion(self, messages):
+        self._reset_stats()
+        payload = self._options.copy()
+        payload['stream'] = True
+        payload['stream_options'] = { 'include_usage': True }     # Required for LiteLLM
+        payload['messages'] = messages
+
+        response = self._session.post(
+            self._base_url + '/v1/chat/completions',
+            json = payload,
+            stream = True,
+            verify = not self._insecure
+        )
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        iterator = response.iter_lines()
+
+        for line_bytes in iterator:
+            line = line_bytes.decode('utf-8').strip()
+            # Standard SSE format: lines start with "data: "
+            if line.startswith('data:'):
+                data_content = line[len('data:'):].strip()
+                if data_content == '[DONE]': # OpenAI-specific end-of-stream marker
+                    response.close()
+                    return
+                chunk = json.loads(data_content)
+                if ('choices' in chunk and
+                    'delta' in chunk['choices'][0] and
+                    'content' in chunk['choices'][0]['delta']):
+                    yield chunk['choices'][0]['delta']['content']
+                self._parse_stats(chunk)
+            # Lines that are not "data: ..." or empty lines are typically ignored in SSE
+            # or could be comments (starting with ':')
+
+class LlmLineStreaming(LlmStreaming):
     def __init__(self, url, api_key=None, options={}, embedding_query='', insecure=False):
         loc = locals()
         del loc['self']
@@ -150,11 +180,11 @@ if __name__ == '__main__':
     ]
 
     print('-- Counting tokens --')
-    llm = Llm(config['openai_url'], config['openai_key'], options, insecure=True)
+    llm = LlmStreaming(config['openai_url'], config['openai_key'], options, insecure=True)
     tokens = llm.count_tokens(prompt)
     print(f'Tokens: {tokens}')
 
-    print('-- Llm --')
+    print('-- LlmStreaming --')
     comp = llm.completion(prompt)
     for token in comp:
         print(f'<{token}>')
