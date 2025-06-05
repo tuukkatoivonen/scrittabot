b961b45987f9475d64459ada468f5b4255cc361c
Tuukka Toivonen <tuukka.toivonen@iki.fi>
2025-06-05 14:25:25 +0300

    llm-replay.py: add utility to "replay" failing requests to LLM

---
+++ llm-replay.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import pprint
+import requests
+import urllib3
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='Override model to use')
+parser.add_argument('--timeout', type=int, help='Set timeout', default=120)
+args = parser.parse_args()
+
+config = yaml.safe_load(open('config.yaml', 'r'))
+payload = json.load(open('llm_exception_payload.json'))
+timeout = args.timeout
+
+if args.model:
+    payload['model'] = args.model
+
+url = config['openai_url'] + '/v1/chat/completions'
+headers = { 'Content-Type': 'application/json' }
+if 'openai_key' in config:
+    headers['Authorization'] = f'Bearer {config["openai_key"]}'
+
+urllib3.disable_warnings()  # Suppress all warnings from urllib3 (which requests uses)
+response = requests.post(url, json=payload, headers=headers, timeout=timeout, verify=False)
+pprint.pp(response.json())
+response.raise_for_status()  # Raises HTTPError for bad responses (4XX or 5XX)
